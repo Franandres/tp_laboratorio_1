7ca7d7925657bc0b62e9e25bdae9d28f2a842af5
Franandres <fran.loras98@gmail.com>
2020-09-12 17:22:20 -0300

    Calculadora

--- Calculadora/Operaciones.h
+++ Calculadora/Operaciones.h
@@ -1,6 +1,51 @@
+/** La suma de dos numeros
+ *
+ * float el primer operando
+ * float el segundo operando
+ *
+ * float devuelve la suma de dos numeros
+ */
 double SumaDeOperandos (float, float, float);
+
+// ------------------------------------------
+
+/** La resta de dos numeros
+ *
+ * float el primer operando
+ * float el segundo operando
+ *
+ * float devuelve la resta de dos numeros
+ */
 double RestaDeOperandos (float, float, float);
+
+// ------------------------------------------
+
+/** La division de dos numeros
+ *
+ * float el dividendo
+ * float el divisor
+ *
+ * float devuelve el cociente del dividendo
+ */
 double DivisionDeOperandos (float, float, float);
+
+// ------------------------------------------
+
+/** La multiplicacion de dos numeros
+ *
+ * float el primer factor
+ * float el segundo factor
+ *
+ * float devuelve la producto de dos numeros
+ */
 double MultiplicacionDeOperandos (float, float, float);
+
+// ------------------------------------------
+
+/** El factorial de un numero
+ *
+ * float el numero a ser factorizado
+ *
+ * float devuelve la el factorial de un numero
+ */
 double FactorialDeOperando (float, float);
-double ResultadoDeOperaciones (float);
